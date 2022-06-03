7fb3bdad99c93e3699cc737d63fe8bcab391c2ba
geonwoo <yung65322136@gmail.com>
2022-06-03 23:41:57 +0900

    Initial commit

---
+++ src/test/java/com/geonoo/board/SimpleBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.geonoo.board;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleBoardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
